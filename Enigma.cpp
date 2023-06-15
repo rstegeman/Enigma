e3e100c57c767cb56cff27d67d34fe05ef8beb48
Robbie Stegeman <rstegeman@mandli.com>
2023-06-15 10:45:42 -0500

    Added rotor configs

--- Enigma.cpp
+++ Enigma.cpp
@@ -4,79 +4,110 @@ using namespace std;
 char plugIn[6];
 char plugOut[6];
 
-void encrypt() {
-    cout << "-------------------Enigma Machine-------------------" << endl;
-    cout << "-----------------Plugboard Settings-----------------" << endl;
-    cout << "1. Use default" << endl; //default plugboard settings
-    cout << "2. Use custom" << endl; //custom plugboard settings
-    int response;
-    cin >> response;
-    cout << endl;
+const char rotor0[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+const char rotor1[] = {'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A'};
+const char rotor2[] = {'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B'};
+const char rotor3[] = {'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C'};
+const char rotor4[] = {'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D'};
+const char rotor5[] = {'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E'};
+const char rotor6[] = {'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F'};
+const char rotor7[] = {'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+const char rotor8[] = {'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+const char rotor9[] = {'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+const char rotor10[] = {'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+const char rotor11[] = {'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'};
+const char rotor12[] = {'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'};
+const char rotor13[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+const char rotor14[] = {'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'};
+const char rotor15[] = {'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'};
+const char rotor16[] = {'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'};
+const char rotor17[] = {'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q'};
+const char rotor18[] = {'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'};
+const char rotor19[] = {'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S'};
+const char rotor20[] = {'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'};
+const char rotor21[] = {'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'};
+const char rotor22[] = {'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'};
+const char rotor23[] = {'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W'};
+const char rotor24[] = {'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X'};
+const char rotor25[] = {'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'};
 
-    while(1) {
-    if ((response != 1 && response != 2) || cin.fail()) {
+
+void encrypt() {
+  cout << "-------------------Enigma Machine-------------------" << endl;
+  cout << "-----------------Plugboard Settings-----------------" << endl;
+  cout << "1. Use default" << endl; //default plugboard settings
+  cout << "2. Use custom" << endl; //custom plugboard settings
+  int response;
+  cin >> response;
+  cout << endl;
+
+  while(1) {
+    if ((response != 1 && response != 2) || cin.fail()) { //Catch erroneous responses
       cin.clear();
       cout << "Please enter either 1 or 2." << endl;
       cin.ignore(numeric_limits<streamsize>::max(), '\n');
       cin >> response;
       cout << endl;
     }
-    if (response == 1 || response == 2 || !cin.fail()) {
+    if (response == 1 || response == 2 || !cin.fail()) { //corrected input
       break;
     }
+  }
 
-    if (response == 1) {
-      *(plugIn) = 'A';
-      *(plugIn + 1) = 'P';
-      *(plugIn + 2) = 'T';
-      *(plugIn + 3) = 'B';
-      *(plugIn + 4) = 'K';
-      *(plugIn + 5) = 'O';
+  if (response == 1) { //default plugboard settings
+    *(plugIn) = 'A';
+    *(plugIn + 1) = 'P';
+    *(plugIn + 2) = 'T';
+    *(plugIn + 3) = 'B';
+    *(plugIn + 4) = 'K';
+    *(plugIn + 5) = 'O';
 
-      *(plugOut) = 'L';
-      *(plugOut + 1) = 'R';
-      *(plugOut + 2) = 'D';
-      *(plugOut + 3) = 'W';
-      *(plugOut + 4) = 'F';
-      *(plugOut + 5) = 'Y';
-    } else {
-      for (int i = 0; i < 6; i++) {
-        cout << "Please enter a unique capital letter. (" << i + 1 << " of 6)" << endl;
-        char letterIn;
-        cin >> letterIn;
-        cout << endl;
+    *(plugOut) = 'L';
+    *(plugOut + 1) = 'R';
+    *(plugOut + 2) = 'D';
+    *(plugOut + 3) = 'W';
+    *(plugOut + 4) = 'F';
+    *(plugOut + 5) = 'Y';
+  } else {
+    for (int i = 0; i < 6; i++) {
+      cout << "Please enter a unique capital letter. (" << i + 1 << " of 6)" << endl; //custom plugboard input
+      char letterIn;
+      cin >> letterIn;
+      cout << endl;
 
-        while(1) {
-        if ((letterIn < 'A' && letterIn > 'Z') || cin.fail()) {
+      while(1) {
+        if ((letterIn < 'A' && letterIn > 'Z') || cin.fail()) { //erroneous plugboard input
           cin.clear();
           cout << "Please enter a unique capital letter." << endl;
           cin.ignore(numeric_limits<streamsize>::max(), '\n');
           cin >> letterIn;
           cout << endl;
         }
-        if (letterIn >= 'A' || letterIn <= 'Z' || !cin.fail()) {
+        if (letterIn >= 'A' || letterIn <= 'Z' || !cin.fail()) { //corrected input
           break;
         }
       }
     }
+  }
 }
 
 void decrypt() {
-    
+    cout << "-------------------Enigma Machine-------------------" << endl;
+    cout << "-----------------Plugboard Settings-----------------" << endl;
 }
 
 int main() {
-    cout << "-------------------Enigma Machine-------------------" << endl;
-    cout << "Which would you like to do?:" << endl;
-    cout << "1. Encrypt" << endl;
-    cout << "2. Decrypt" << endl;
-    cout << endl;
+  cout << "-------------------Enigma Machine-------------------" << endl;
+  cout << "Which would you like to do?:" << endl;
+  cout << "1. Encrypt" << endl;
+  cout << "2. Decrypt" << endl;
+  cout << endl;
 
-    int response;
-    cin >> response;
-    cout << endl;
+  int response;
+  cin >> response;
+  cout << endl;
 
-    while(1) {
+  while(1) {
     if ((response != 1 && response != 2) || cin.fail()) {
       cin.clear();
       cout << "Please enter either 1 or 2." << endl;
@@ -94,4 +125,5 @@ int main() {
         decrypt();
     }
     return 0;
+  }
 }
