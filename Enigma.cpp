77993f9ec860b9916a4b8710dcc2f3244358d2a5
Robbie Stegeman <rstegeman@mandli.com>
2023-06-16 17:36:44 -0500

    Cleaned up some of the messages, optimized

--- Enigma.cpp
+++ Enigma.cpp
@@ -162,14 +162,57 @@ string caesarCipher(string message) {
 }
 
 void setPlugboard() {
-  cout << "-----------------Plugboard Settings-----------------" << endl;
-  cout << "Enter provide the upper reflection: " << endl;
+  cout << "-------------------Plugboard Settings-------------------" << endl << endl;
+  cout << "Positive code: ";
   string upper;
   cin >> upper;
 
-  cout << "Enter provide the lower reflection: " << endl;
+  for (int i = 0; i < upper.size(); i++) { //Catch any messages that contain non-letters
+    if ( ((upper[i] < 'a' || upper[i] > 'z') && (upper[i] < 'A' || upper[i] > 'Z')) || cin.fail() || upper.size() != 10) {
+      cin.clear();
+      cout << "Please enter a valid positive code." << endl;
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cin >> upper;
+      cout << endl;
+      i = -1;
+    }
+    for (int j = i + 1; j < upper.size(); j++) {
+      if (upper[i] == upper[j]) {
+        cin.clear();
+        cout << "Please enter a valid positive code." << endl;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin >> upper;
+        cout << endl;
+        i = -1;
+      }
+    }
+  }
+
+  cout << "Negative code: ";
   string lower;
   cin >> lower;
+
+  for (int i = 0; i < lower.size(); i++) { //Catch any messages that contain non-letters
+    if ( ((lower[i] < 'a' || lower[i] > 'z') && (lower[i] < 'A' || lower[i] > 'Z')) || cin.fail() || lower.size() != 10) {
+      cin.clear();
+      cout << "Please enter a valid negative code." << endl;
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cin >> lower;
+      cout << endl;
+      i = -1;
+    }
+    for (int j = i + 1; j < lower.size(); j++) {
+      if (lower[i] == lower[j]) {
+        cin.clear();
+        cout << "Please enter a valid negative code." << endl;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin >> lower;
+        cout << endl;
+        i = -1;
+      }
+    }
+  }
+  cout << endl;
 
   for (int i = 0; i < num_pairs; i++) {
     plugOne[i] = upper[i];
@@ -180,30 +223,31 @@ void setPlugboard() {
 }
 
 void encrypt() {
-  cout << "-------------------Enigma Machine-------------------" << endl;
-  cout << "-----------------Plugboard Settings-----------------" << endl;
+  cout << "-------------------Plugboard Settings-------------------" << endl;
   generateRandomPlugboard(); //create randomized plugboard settings
-  cout << "A randomized plugboard pairings have been created for you." << endl; //default plugboard settings
-  cout << "The following letters have been swapped:" << endl; //custom plugboard settings
+  cout << "Randomized plugboard settings have been created for you." << endl << endl;
+  cout << "Positive code: ";
   for (int i = 0; i < num_pairs; i++) {
     cout << plugOne[i]; 
   }
-  cout << " <- Upper Reflection"<< endl;
+  cout << endl;
 
+  cout << "Negative code: ";
   for (int i = 0; i < num_pairs; i++) {
     cout << plugTwo[i]; 
   }
-  cout << " <- Lower Reflection"<< endl;
+  cout << endl;
 
   cout << endl;
   generateRandomRotors();
-  cout << "-------------------Rotor Settings-------------------" << endl;
-  cout << "Randomized rotor settings have been created for you." << endl;
-  cout << "The first rotor is set to: " << (char)(rotorOneID + 'A') << endl;
-  cout << "The second rotor is set to: " << (char)(rotorTwoID + 'A') << endl;
-  cout << "The third rotor is set to: " << (char)(rotorThreeID + 'A') << endl;
+  cout << "---------------------Rotor Settings--------------------" << endl;
+  cout << "Randomized rotor settings have been created for you." << endl << endl;
+  cout << "First rotor: " << (char)(rotorOneID + 'A') << endl;
+  cout << "Second rotor: " << (char)(rotorTwoID + 'A') << endl;
+  cout << "Third rotor: " << (char)(rotorThreeID + 'A') << endl;
   cout << endl;
 
+  cout << "------------------------Message-------------------------" << endl;
   cout << "What is the message you would like to encrypt?" << endl;
   string message;
   cin >> message;
@@ -229,16 +273,15 @@ void encrypt() {
   message = plugboardSwap(message);
   message = caesarCipher(message);
 
-  cout << "-------------------Enigma Machine-------------------" << endl;
-  cout << "Your encrypted message: " << message << endl;
+  cout << "------------------------Encrypted-----------------------" << endl;
+  cout << "Your encrypted message: " << endl << message << endl;
 
 }
 
 void decrypt() {
-    cout << "-------------------Enigma Machine-------------------" << endl;
     setPlugboard();
-    cout << "-------------------Rotor Settings-------------------" << endl;
-    cout << "What letter should the first rotor be set to?"  << endl;
+    cout << "---------------------Rotor Settings---------------------" << endl << endl;
+    cout << "First rotor: ";
     char rotor;
     cin >> rotor;
 
@@ -259,7 +302,7 @@ void decrypt() {
     }
 
     rotorOneID = rotor - 'A';
-    cout << "What letter should the second rotor be set to?"  << endl;
+    cout << "Second rotor: ";
     cin >> rotor;
 
     while(1) {
@@ -279,7 +322,7 @@ void decrypt() {
     }
 
     rotorTwoID = rotor - 'A';
-    cout << "What letter should the third rotor be set to?"  << endl;
+    cout << "Third rotor: ";
     cin >> rotor;
 
     while(1) {
@@ -298,50 +341,78 @@ void decrypt() {
       rotor = rotor + 'A' - 'a';
     }
 
+    cout << endl;
+
     rotorThreeID = rotor - 'A';
-    cout << "----------------------Message-----------------------" << endl;
+    cout << "------------------------Message-------------------------" << endl;
     cout << "What is your encrypted message?" << endl;
     string message;
     cin >> message;
+
+    for (int i = 0; i < message.size(); i++) { //Catch any messages that contain non-letters
+      if ( ((message[i] < 'a' || message[i] > 'z') && (message[i] < 'A' || message[i] > 'Z')) || cin.fail() ) {
+        cin.clear();
+        cout << "Please use letters only." << endl;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin >> message;
+        cout << endl;
+        i = -1;
+      }
+    }
+
+    cout << endl;
 
 
     message = caesarDecipher(message);
     message = plugboardSwap(message);
 
-    cout << "-------------------Enigma Machine-------------------" << endl;
-    cout << "Your decrypted message: " << message << endl;
+    cout << "------------------------Decrypted-----------------------" << endl;
+    cout << "Your decrypted message: " << endl << message << endl;
 
 
 }
 
 int main() {
-  cout << "-------------------Enigma Machine-------------------" << endl;
-  cout << "Which would you like to do?:" << endl;
-  cout << "1. Encrypt" << endl;
-  cout << "2. Decrypt" << endl;
-
-  int response;
-  cin >> response;
-  cout << endl;
-
   while(1) {
-    if ((response != 1 && response != 2) || cin.fail()) {
-      cin.clear();
-      cout << "Please enter either 1 or 2." << endl;
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      cin >> response;
-      cout << endl;
-    }
-    if ((response == 1 || response == 2) && !cin.fail()) {
-      break;
-    }
-  }
+    cout << endl;
+    cout << "    ______      _                          __  ___           __    _          " << endl;
+    cout << "   / ____/___  (_)___ _____ ___  ____ _   /  |/  /___ ______/ /_  (_)___  ___ " << endl;
+    cout << "  / __/ / __ \\/ / __ `/ __ `__ \\/ __ `/  / /|_/ / __ `/ ___/ __ \\/ / __ \\/ _ \\" << endl;
+    cout << " / /___/ / / / / /_/ / / / / / / /_/ /  / /  / / /_/ / /__/ / / / / / / /  __/" << endl;
+    cout << "/_____/_/ /_/_/\\__, /_/ /_/ /_/\\__,_/  /_/  /_/\\__,_/\\___/_/ /_/_/_/ /_/\\___/ " << endl;
+    cout << "              /____/                                                          " << endl;
+    cout << "Which would you like to do?" << endl;
+    cout << "1. Encrypt" << endl;
+    cout << "2. Decrypt" << endl;
+    cout << "3. Exit" << endl << endl;
 
-    if (response == 1) {
-        encrypt();
-    } else {
-        decrypt();
+    int response;
+    cin >> response;
+    cout << endl;
+
+    while(1) {
+      if ((response != 1 && response != 2 && response != 3) || cin.fail()) {
+        cin.clear();
+        cout << "Please enter either 1, 2, or 3." << endl;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cin >> response;
+        cout << endl;
+      }
+      if ((response == 1 || response == 2 || response == 3) && !cin.fail()) {
+        break;
+      }
     }
-    return 0;
+
+      if (response == 1) {
+          encrypt();
+      } else if (response == 2) {
+          decrypt();
+      } else if (response == 3) {
+        return 0;
+      } else {
+        return 1;
+      }
+      
+  }
+  
 }
-//cout << "Checkpoint" << endl;
